d804a82884fe32f9eddc89c6c1c04ef8567f3896
Ed Schouten <ed@nuxi.nl>
2015-08-10 14:07:23 +0200

    Don't forget to convert the errno.

--- sys/compat/cloudabi/cloudabi_sock.c
+++ sys/compat/cloudabi/cloudabi_sock.c
@@ -361,7 +361,7 @@ cloudabi_sys_sock_stat_get(struct lwp *l,
 	}
 
 	/* Set ss_error. */
-	ss.ss_error = so->so_error;
+	ss.ss_error = cloudabi_convert_errno(so->so_error);
 	if ((SCARG(uap, flags) & CLOUDABI_SOCKSTAT_CLEAR_ERROR) != 0)
 		so->so_error = 0;
 
